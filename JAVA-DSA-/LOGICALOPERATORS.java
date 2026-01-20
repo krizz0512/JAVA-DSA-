1b0eb41e3cb21d6a86561dc4857f60203a141db0
Krish Mishra <krishmisra123@gmail.com>
2026-01-21 01:08:14 +0530

    Add Dsa files

---
+++ JAVA-DSA-/LOGICALOPERATORS.java
@@ -0,0 +1,7 @@
+public class LOGICALOPERATORS {
+    public static void main(String args[]) {
+       // AND OP.: System.out.println ( (1>2) && (5>0));
+      // OR OP.  System.out.println( (1>2) || (5<0) ) ;
+   System.out.println( !(0>5));
+ }
+}
