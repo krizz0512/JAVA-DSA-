4c1cb2e1ac9dc3783c8f74c5eecc3112c842aab9
Krish Mishra <krishmisra123@gmail.com>
2026-01-21 00:35:51 +0530

    Area of a circle in java

---
+++ JAVABASICS.java
@@ -0,0 +1,23 @@
+
+
+public class JAVABASICS {
+   public static void main(String args []) {
+      //int a =10;//
+     // float b =20.25f;
+     // long c = 25;//
+      //double d = 30;
+      //float ans = (float) (a + b + c + d);
+      //System.out.println(ans);
+
+      int a= 65;
+      double d= 56;
+      float c = 69.99f;
+      short b= 58;
+      int ans = (int) (a+b+c+d);
+      System.out.println(ans);     
+
+
+   }
+}
+   
+ 
