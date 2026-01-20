ee6e5e956ac4c3dd896114270ac5060c6cb73abf
Krish Mishra <krishmisra123@gmail.com>
2026-01-21 01:36:40 +0530

    move files into Basics folder

--- DSA/Basics/LOGICALOPERATORS.java
+++
@@ -1,7 +0,0 @@
-public class LOGICALOPERATORS {
-    public static void main(String args[]) {
-       // AND OP.: System.out.println ( (1>2) && (5>0));
-      // OR OP.  System.out.println( (1>2) || (5<0) ) ;
-   System.out.println( !(0>5));
- }
-}
