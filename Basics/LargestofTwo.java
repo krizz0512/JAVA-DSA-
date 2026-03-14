a3046092ab848aafa1dcceb6142690a0eafcb29c
Krish Mishra <krishmisra123@gmail.com>
2026-03-15 01:00:22 +0530

    largest of two nos.

---
+++ Basics/LargestofTwo.java
@@ -0,0 +1,14 @@
+public class LargestofTwo{
+    public static void main(String[] args) {
+        int A = 1;
+        int B = 5;
+
+       if (A>=B){
+        System.out.println("A");
+    }
+    else{
+        System.out.println("B");
+    }
+   
+    }
+}
