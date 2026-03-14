88a629ed79f94834668c7f2a39718cbad875441f
Krish Mishra <krishmisra123@gmail.com>
2026-03-15 00:47:19 +0530

    Conditional Statement

---
+++ Basics/ConditionalStatement.java
@@ -0,0 +1,12 @@
+
+public class ConditionalStatement{
+    public static void main(String[] args) {
+    int age=22;
+    if(age >= 18){
+        System.out.println("You are eligilbe to vote");
+    }
+    else{
+        System.out.println("You are not eligible to vote");
+    }
+}
+}
