d551cdde05429c2a798513bddc13f4da615925b2
Krish Mishra <krishmisra123@gmail.com>
2026-03-16 00:59:45 +0530

    ternary operators in java

---
+++ Basics/ternaryoperator.java
@@ -0,0 +1,8 @@
+public class ternaryoperator {
+    public static void main(String[] args) {
+        int number =2;
+        //ternary operator
+        String type = ((number%2) == 0) ? "even" : "odd";
+        System.out.println(type); 
+    }
+}
