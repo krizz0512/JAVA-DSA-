0d628de4554617db50b07f161d81f4fb7ed5f7b9
Krish Mishra <krishmisra123@gmail.com>
2026-03-15 23:50:41 +0530

    elseif

---
+++ Basics/ElseIf.java
@@ -0,0 +1,16 @@
+public class ElseIf {
+public static void main(String[] args) {
+    int age=22;
+    if(age>=18){
+        System.out.println("adult");
+    } 
+    else if (age>=13 && age<18) {
+        System.out.println("teenager");
+        
+    }
+    else {
+        System.out.println("child");
+    }
+}
+    
+}
