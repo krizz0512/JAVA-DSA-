6dbab96c930ab6ca3a1b537fa30658b9e6242c4e
Krish Mishra <krishmisra123@gmail.com>
2026-03-16 00:41:13 +0530

    largestofthree nos.

---
+++ Basics/largestofthree.java
@@ -0,0 +1,16 @@
+public class largestofthree {
+    public static void main(String[] args) {
+        int A= 1;
+        int B= 3;
+        int C= 6;
+        if((A>=B) && (B>=C)){
+            System.out.println("A");
+        }
+        else if(B>=C){
+         System.out.println("B");
+        }
+        else{
+            System.out.println("C");
+        }
+    }
+}
