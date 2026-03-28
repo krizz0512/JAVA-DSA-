1bcfbe0560deae4a1c738df283bd09cc892a94c0
Krish Mishra <krishmisra123@gmail.com>
2026-03-29 02:12:56 +0530

    square pattern

---
+++ Basics/squarepattern.java
@@ -0,0 +1,13 @@
+public class squarepattern {
+    public static void main(String[] args) {
+        for(int lines=1; lines<=4; lines++)
+            System.out.println("****");
+
+        //for while loop we'll do this:
+        //int line =1
+        //while (line<=4){
+        //System.out.println("****");
+        //line++;
+    //    }
+    }
+}
