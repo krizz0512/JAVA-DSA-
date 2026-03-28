94867ca75c5bfd2f767e83027013297ce0aeb5f7
Krish Mishra <krishmisra123@gmail.com>
2026-03-29 03:53:28 +0530

    Break Statement

---
+++ Loops/Breakstatemnt.java
@@ -0,0 +1,12 @@
+public class Breakstatemnt {
+    public static void main(String[] args) {
+        for( int i=1 ; i<=5; i++){
+            if(i==3){
+                break; 
+            }
+            System.out.println(i);
+
+        }
+        System.out.println("I AM out of loop");
+    }
+}
