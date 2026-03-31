ee9e5df317269c076386bc951edc21597aa56bb6
Krish Mishra <krishmisra123@gmail.com>
2026-04-01 02:44:16 +0530

    continue statement 1

--- Loops/continuestamnt.java
+++ Loops/continuestamnt.java
@@ -1,4 +1,4 @@
-public class continuesatmnt {
+public class continuestamnt {
     public static void main(String[] args) {
         for(int i=1; i<=5; i++){
             if(i==3){ //this thing use to skip iteration like we have to print 1 2 3 4 5 and skip 3
