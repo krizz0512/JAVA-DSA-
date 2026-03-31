ddf29da514e302364ce47060cf520114017f3817
Krish Mishra <krishmisra123@gmail.com>
2026-04-01 03:33:54 +0530

    prime or not

---
+++ Loops/primeornot.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class primeornot {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        if (n==2){
+            System.out.println("n is prime");
+        } else{
+            boolean isPrime= true;    
+            for(int i = 2; i<=Math.sqrt(n); i++){ // refer to notebook (root n number times multiple will be unique)
+                if (n% i ==0){ //n is a multiple of i(i not equal to 1 or n)
+                    isPrime = false;
+                }
+            }
+            if(isPrime == true){
+                System.out.println("n is prime");
+
+            } else{
+                System.out.println("n is not prime");
+            }
+        }
+    }
+}
