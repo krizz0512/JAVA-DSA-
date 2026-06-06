06db73d017c4c992138cfeefcda3e00c04922b61
Krish Mishra <krishmisra123@gmail.com>
2026-06-07 03:23:38 +0530

    input and output of array

---
+++ Arrays/inputandoutput.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class inputandoutput {
+    public static void main(String[] args) {
+        int marks[]=new int[100];
+        Scanner sc = new Scanner(System.in);
+        //int phy;
+        //phy=sc.nextInt();
+
+        marks[0]=sc.nextInt(); //phy
+        marks[1]=sc.nextInt(); //chem
+        marks[2]=sc.nextInt(); //math
+
+        System.out.println("phy:"+marks[0]);
+        System.out.println("chem:"+marks[1]);
+        System.out.println("math:"+marks[2]);
+
+        marks [1]=marks[1]+1;//updated  1st index value by adding 1 no.
+        System.out.println("chem:"+marks[1]);
+        int percentage=(marks[0]+ marks[1]+marks[2]) /3;
+        System.out.println("percentage =" + percentage+ "%");
+        
+
+
+
+    
+}
+
+    
+}
